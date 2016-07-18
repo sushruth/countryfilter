098580fe47255587b86012172e93645bf8fc8ebf
Sushruth Sastry <Sushruth Sastry>
2016-07-18 16:36:01 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+console.log(angular);
+
+var myapp = angular.module('myApp', []);
+
+myapp.controller('myCtrl', function ($scope, $http) {
+	$scope.stateData = [];
+	$scope.filterVal = 'A';
+	$http.get('https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_hash.json').then((response) => {
+		var data = response.data;
+		for (state in data) {
+			$scope.stateData.push({ short: state, long: data[state] })
+		}
+		console.log($scope.stateData);
+	});
+	$scope.originalData = $scope.stateData;
+	$scope.onchangelistener = () => {
+
+	};
+})
+
+myapp.filter('letterFilter', function () {
+	return function (items, letters) {
+		var m = [];
+		var matcher = new RegExp(letters, 'i');
+		items = items.map((v) => {
+			if (v.long.substr(0,letters.length).match(matcher)) {
+				m.push(v);
+				return v;
+			}
+		})
+		return m;
+	}
+})
